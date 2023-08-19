7a18f3535c62bda6f623b0964b1f05a96f56a28f
finn <finnmorrison29@gmail.com>
2023-08-18 23:26:14 -0700

    added upload json

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -4,31 +4,67 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
 import useACData from '../hooks/useACData.js';
+
+// icons
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 export default function CharacterAppBar() {
     const [ACData, setACData] = useACData();
+    const [editingName, setEditingName] = React.useState(false);
+
+    const fileInputRef = React.useRef(null);
+
+    React.useEffect(() => {
+        if (editingName) {
+            document.getElementById('name-input').focus();
+        }
+    }, [editingName]);
+
+    const fileReader = new FileReader();
+
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            console.log("selected file: ", file);
+
+            fileReader.onload = (e) => {
+                const json = JSON.parse(e.target.result);
+                console.log("json: ", json);
+                setACData(json);
+            }
+            fileReader.readAsText(file);
+        }
+    }
+
+    // Proxy the click event to the hidden file input element
+    const handleFileUploadButtonClicked = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    }
 
     return (
         <Box sx={{ flexGrow: 1}}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
+                    {!editingName && <Typography
+                        sx={{ flexGrow: 1 }}
+                        onClick={() => {
+                            setEditingName(true);
+                        }}
                     >
-                        <MenuIcon />
-                    </IconButton>
-                    <TextField
-                        sx={{flexGrow: 1}}
+                        {ACData.name || 'Enter Character Name'}
+                    </Typography>}
+                    {editingName && <TextField
+                        id="name-input"
+                        sx={{flexGrow: 1, color: 'white'}}
                         placeholder='Character Name'
+                        inputProps={
+                            {style: {color: 'white'}}
+                        }
                         onKeyDown={(e) => {
                             if (e.key === 'Enter') {
                                 e.target.blur();
@@ -36,12 +72,32 @@ export default function CharacterAppBar() {
                         }}
                         onBlur={(e) => {
                             setACData({...ACData, name: e.target.value});
+                            setEditingName(false);
                         }}
                         onChange={(e) => {
                             setACData({...ACData, name: e.target.value});
                         }}
+                        value={ACData.name}
+                    />}
+                    <input
+                        ref={fileInputRef}
+                        accept=".json"
+                        id="file-upload-button"
+                        type="file"
+                        style={{display: 'none'}}
+                        onChange={handleFileInputChange}
                     />
-                    <Button color="inherit">Login</Button>
+                    <IconButton
+                        color="inherit"
+                        aria-label='upload'
+                        title="upload"
+                        onClick={handleFileUploadButtonClicked}
+                    >
+                        <FileUploadIcon />
+                    </IconButton>
+                    <IconButton color="inherit" aria-label='download' title="download">
+                        <FileDownloadIcon />
+                    </IconButton>
                 </Toolbar>
             </AppBar>
         </Box>
